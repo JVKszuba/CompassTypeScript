ad7d683e83036b77498f4f2d8542069862fc10a9
JVKszuba <jvkaszuba@gmail.com>
2025-07-25 11:44:10 -0300

    feat: add CourseGoals component

--- Frameworks/react-demo/src/App.tsx
+++ Frameworks/react-demo/src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
+import CourseGoals from "./components/CourseGoals.tsx";
 
 function App() {
 
@@ -8,6 +9,8 @@ function App() {
         <Header image={{src: goalsImg, alt: 'A list of goals'}}>
             <h1>Your Course Goals</h1>
         </Header>
+        <CourseGoals goals={[{id: 1, title: 'Learn Ts', description: 'Learn Ts from the ground up'},
+                             {id: 2, title: 'Practice Ts', description: 'Practice working with Ts'}]} />
     </main>
   )
 }
---
+++ Frameworks/react-demo/src/components/CourseGoals.tsx
@@ -0,0 +1,30 @@
+type Goal = {
+
+    id: number;
+    title: string;
+    description: string;
+}
+
+interface CourseGoalsProps {
+
+    goals: Goal[];
+}
+
+export default function CourseGoals({goals}: CourseGoalsProps) {
+
+    return (
+        <ul>
+            {goals.map((goal: Goal) =>
+                <li key={goal.id}>
+                    <article>
+                        <div>
+                            <h2>{goal.title}</h2>
+                            <p>{goal.description}</p>
+                        </div>
+                        <button>Delete</button>
+                    </article>
+                </li>
+            )}
+        </ul>
+    )
+}
