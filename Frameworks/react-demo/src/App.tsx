3c746aeb48742ff108f60332a3030c4d5807b643
JVKszuba <jvkaszuba@gmail.com>
2025-07-25 11:54:44 -0300

    feat: add Goals deletion logic to delete button

--- Frameworks/react-demo/src/App.tsx
+++ Frameworks/react-demo/src/App.tsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
 import CourseGoals from "./components/CourseGoals.tsx";
 
 function App() {
 
+    const [goals, setGoals] = useState([
+        {id: 1, title: 'Learn Ts', description: 'Learn Ts from the ground up'},
+        {id: 2, title: 'Practice Ts', description: 'Practice working with Ts'}
+    ]);
+
+    function handleDeleteGoal(id: number) {
+
+        setGoals((prevGoals) => prevGoals.filter((g) => g.id !== id));
+    }
+
   return (
     <main>
         <Header image={{src: goalsImg, alt: 'A list of goals'}}>
             <h1>Your Course Goals</h1>
         </Header>
-        <CourseGoals goals={[{id: 1, title: 'Learn Ts', description: 'Learn Ts from the ground up'},
-                             {id: 2, title: 'Practice Ts', description: 'Practice working with Ts'}]} />
+        <CourseGoals goals={goals} onDelete={handleDeleteGoal} />
     </main>
   )
 }
--- Frameworks/react-demo/src/components/CourseGoals.tsx
+++ Frameworks/react-demo/src/components/CourseGoals.tsx
@@ -8,9 +8,10 @@ type Goal = {
 interface CourseGoalsProps {
 
     goals: Goal[];
+    onDelete: (id: number) => void;
 }
 
-export default function CourseGoals({goals}: CourseGoalsProps) {
+export default function CourseGoals({goals, onDelete}: CourseGoalsProps) {
 
     return (
         <ul>
@@ -21,7 +22,7 @@ export default function CourseGoals({goals}: CourseGoalsProps) {
                             <h2>{goal.title}</h2>
                             <p>{goal.description}</p>
                         </div>
-                        <button>Delete</button>
+                        <button onClick={() => onDelete(goal.id)}>Delete</button>
                     </article>
                 </li>
             )}
